562bb65e99855b2fb09a26f4088122777410c10d
Arian0240 <128600124+ArianYN@users.noreply.github.com>
2024-02-19 10:03:17 +0100

    Added User Classes
    Here, i've added the classes for our users. Their names, phonenumbers, email, etc.

---
+++ Domain Models/UserData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain_Models
+{
+    internal class UserData
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Phonenumber { get; set; }
+        public bool Newsletter { get; set; }
+    }
+}
---
+++ Domain Models/UserProfiles.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain_Models
+{
+    public class UserProfiles
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int BirthDate { get; set; }
+        public int Reputation {  get; set; }
+        public int Selling {  get; set; }
+    }
+}
