9c0e13b7e5f848e0eb800c5e0748d97ba9d52dec
Alexander Piilgaard <alex344b@edu.mercantec.dk>
2024-03-19 11:07:09 +0100

    Added product_type

--- ProductClassLibrary/ProductParent.cs
+++ ProductClassLibrary/ProductParent.cs
@@ -6,6 +6,7 @@
         public string Brand { get; set; }
         public string Model { get; set; }
         public string Series { get; set; }
+        public string Product_Type { get; set; }
 
     }
 }
