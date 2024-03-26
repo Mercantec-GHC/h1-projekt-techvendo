03a449e0d09965b332bc989966a6a2596c2e3fb6
ArianYN <128600124+ArianYN@users.noreply.github.com>
2024-03-26 11:06:10 +0100

    Login System Almost working. Problem with navbar not updating until you press login again, and THEN it shows the correct name

---
+++ BlazorApp/Components/LoginState.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Primitives;
+using System.ComponentModel;
+
+namespace BlazorApp.Components
+{
+    public class LoginState : INotifyPropertyChanged
+    {
+        public bool UserLoggedIn { get; set; }
+        public string UserFirstname { get; set; }
+        public string UserLastname { get; set; }
+        public int ProfileId { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void SetLoggedIn(string firstName, string lastName, int profileId, bool loggedIn)
+        {
+            UserLoggedIn = loggedIn;
+            UserFirstname = firstName;
+            UserLastname = lastName;
+            ProfileId = profileId;
+
+            OnPropertyChanged(nameof(UserLoggedIn));
+            OnPropertyChanged(nameof(UserFirstname));
+            OnPropertyChanged(nameof(UserLastname));
+            OnPropertyChanged(nameof(ProfileId));
+        }
+    }
+}
